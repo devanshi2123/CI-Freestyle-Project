77dc3feddf6717f2053d81e846a05c7759d19b5e
Devanshi Shah <devanshi.shah.sw@gmail.com>
2025-09-05 00:07:03 +0530

    Update main.py to generate output.txt

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# main.py
 def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    result = add(2, 3)
-    with open("output.txt", "w") as f:
-        f.write(f"Result: {result}\n")
+    result = add(10, 30)
     print(f"Result: {result}")
+
+    # Save result to output.txt
+    with open("output.txt", "w") as f:
+        f.write(f"Addition result is: {result}\n")
